b3caa3636b29875f58d7a14dba15f3e371637064
Kooreis <kooreis.official@gmail.com>
2022-10-28 22:26:34 -0500

    The second part of the script adds three methods to the Puzzle class: isSolved, swap, and clone. The isSolved method checks if the puzzle is solved. The swap method swaps the empty tile with another tile. The clone method creates a new Puzzle instance with the same board state.

--- solution.cs
+++ solution.cs
@@ -1,13 +1,21 @@
-class Puzzle {
-    constructor(board) {
-        this.board = board.map(row => row.slice());
-        this.empty = [];
+isSolved() {
+        let correct = 1;
         for(let i = 0; i < 3; i++) {
             for(let j = 0; j < 3; j++) {
-                if(this.board[i][j] === 0) {
-                    this.empty = [i, j];
+                if(this.board[i][j] !== correct % 9) {
+                    return false;
                 }
+                correct++;
             }
         }
+        return true;
     }
-}
+
+    swap([x1, y1], [x2, y2]) {
+        [this.board[x1][y1], this.board[x2][y2]] = [this.board[x2][y2], this.board[x1][y1]];
+        this.empty = [x2, y2];
+    }
+
+    clone() {
+        return new Puzzle(this.board);
+    }
